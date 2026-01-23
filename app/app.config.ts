1e7e85970c45daa2b2a859b40c1f94fabcae3a38
Ivan Alvarez <20419834+ivanovertime@users.noreply.github.com>
2026-01-23 17:51:55 -0400

    chore(branding): update site config

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,18 +1,18 @@
 export default defineAppConfig({
   global: {
     picture: {
-      dark: 'https://images.unsplash.com/photo-1701615004837-40d8573b6652?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      light: 'https://images.unsplash.com/photo-1701615004837-40d8573b6652?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      alt: 'My profile picture'
+      dark: '/avatar.jpg',
+      light: '/avatar.jpg',
+      alt: 'Iván Álvarez'
     },
-    meetingLink: 'https://cal.com/',
-    email: 'ui-pro@nuxt.com',
+    meetingLink: 'https://calendar.app.google/mdouT7hy7xS8XUcd9',
+    email: 'alvarezlopezivanenrique@gmail.com',
     available: true
   },
   ui: {
     colors: {
-      primary: 'blue',
-      neutral: 'neutral'
+      primary: 'zinc',
+      neutral: 'neutral',
     },
     pageHero: {
       slots: {
@@ -23,23 +23,18 @@ export default defineAppConfig({
     }
   },
   footer: {
-    credits: `Built with Nuxt UI • © ${new Date().getFullYear()}`,
+    credits: `Ivan Over Time • © ${new Date().getFullYear()}`,
     colorMode: false,
     links: [{
-      'icon': 'i-simple-icons-discord',
-      'to': 'https://go.nuxt.com/discord',
-      'target': '_blank',
-      'aria-label': 'Nuxt on Discord'
-    }, {
-      'icon': 'i-simple-icons-x',
-      'to': 'https://go.nuxt.com/x',
-      'target': '_blank',
-      'aria-label': 'Nuxt on X'
-    }, {
       'icon': 'i-simple-icons-github',
-      'to': 'https://github.com/nuxt/ui',
+      'to': 'https://github.com/ivanovertime',
       'target': '_blank',
-      'aria-label': 'Nuxt UI on GitHub'
+      'aria-label': 'Iván Álvarez on GitHub'
+    }, {
+      'icon': 'i-simple-icons-linkedin',
+      'to': 'https://www.linkedin.com/in/ialvarez93/',
+      'target': '_blank',
+      'aria-label': 'Iván Álvarez on LinkedIn'
     }]
   }
 })
--- app/app.vue
+++ app/app.vue
@@ -18,9 +18,9 @@ useHead({
 })
 
 useSeoMeta({
-  titleTemplate: '%s - Nuxt Portfolio Template',
-  ogImage: 'https://ui.nuxt.com/assets/templates/nuxt/portfolio-light.png',
-  twitterImage: 'https://ui.nuxt.com/assets/templates/nuxt/portfolio-light.png',
+  titleTemplate: '%s - Ivan Over Time',
+  ogImage: '/avatar.jpg',
+  twitterImage: '/avatar.jpg',
   twitterCard: 'summary_large_image'
 })
 
