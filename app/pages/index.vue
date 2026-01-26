7ee2c15dd04522944cca6ac810cf23d9226a1486
Ivan Alvarez <20419834+ivanovertime@users.noreply.github.com>
2026-01-26 10:03:14 -0400

    feat(content): update hero section and work experience details

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -23,10 +23,9 @@ useSeoMeta({
     <LandingHero :page />
     <UPageSection
       :ui="{
-        container: '!pt-0 lg:grid lg:grid-cols-2 lg:gap-8'
+        container: '!pt-0 flex flex-col gap-8'
       }"
     >
-      <LandingAbout :page />
       <LandingWorkExperience :page />
     </UPageSection>
     <LandingBlog :page />
